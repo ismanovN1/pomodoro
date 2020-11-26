e10bd6d34d1c721fc6ce0a063574d9a882171e05
Ismanov Quandiq <ismanov98q@mail.ru>
2020-11-26 11:49:51 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,235 @@
+import React, {useEffect, createRef, useState} from 'react'
+import lavina from './static/lavina.png'
+import DataTime from './static/data'
+import {musicData} from './static/data'
+import useSound from 'use-sound';
+import uvd from './zvuk-na-sms-attractive-sms.mp3';
+import "./App.css"
+
+const App = () => {
+
+    const [play, {stop}] = useSound(uvd);
+    let sound = document.getElementById('sound')
+    let [state, setState] = useState(false)
+    let [Alert, setAlert] = useState('')
+    let [cls, setCls] = useState('none')
+    let [time, setTime] = useState(0)
+    let [ttime, setTtime] = useState('10:40')
+    let [music, setMusic] = useState(musicData[Math.floor(Math.random() * 5) + 1])
+    let [isSearch, setSearch] = useState(true)
+    let [inter, setInter] = useState(true)
+    const [color, setColor] = useState('black')
+
+    useEffect(() => {
+        const date = new Date()
+        const xxz = ttime.split(':')
+        if (date.getHours() == xxz[0] && cls == 'yes' ) {
+            setAlert('Trenirovka qo\'shildi')
+            setTimeout(()=>{
+                setAlert('')
+            },2000)
+            if (date.getMinutes() < xxz[1]) {
+                const id = setTimeout(() => {
+                    sound.play()
+
+                    setTimeout( ()=>{
+                        sound.load()
+                    } , 4*60*1000)
+                }, ((xxz[1] - date.getMinutes()) * 60 - date.getSeconds()) * 1000)
+                return () => clearTimeout(id)
+            }
+        }
+
+        if (date.getHours() < xxz[0] ) {
+            setAlert('Trenirovka qo\'shildi')
+            setTimeout(()=>{
+                setAlert('')
+            },1000)
+            const id = setTimeout(() => {
+                sound.play()
+                setTimeout( ()=>{
+                    sound.load()
+                } , 4*60*1000)
+            }, (((xxz[0] - date.getHours()) * 60 - date.getMinutes()) * 60 - date.getSeconds()) * 1000)
+            return () => clearTimeout(id)
+        }
+
+    }, [ttime])
+    useEffect(() => {
+        console.log(DataTime)
+    }, [])
+
+
+    useEffect(() => {
+        const id = setTimeout(() => {
+
+            if (isSearch) {
+                let x = search()
+                if (x) {
+                    x.res && setTime(x.res)
+                    setSearch(false)
+                    setColor(x.color)
+                    setState(true)
+                } else {
+                    setInter(prev => !prev)
+                }
+            } else {
+                setInter(prev => !prev)
+            }
+
+        }, 2000)
+        return () => clearInterval(id)
+    }, [inter])
+
+    useEffect(() => {
+        if (!isSearch) {
+            let id = setInterval(
+                () => {
+
+                    if (time > 0) {
+                        setTime(time -= 1)
+                    } else {
+                        document.getElementById('audio').play()
+                        setTimeout(document.getElementById('audio').load, 5000)
+                        setSearch(true)
+                        setInter(prev => !prev)
+                        setColor("black")
+                        setState(false)
+                    }
+
+                }, 1000
+            )
+            return () => {
+                clearInterval(id)
+            }
+        }
+    }, [state, isSearch])
+
+    const search = () => {
+        const timeNow = new Date()
+        const hoursNow = timeNow.getHours()
+        const minutesNow = timeNow.getMinutes()
+        const secondsNow = timeNow.getSeconds()
+        let result
+
+
+        DataTime.forEach(i => {
+            const Start = i && i.start.split(':')
+            const End = i && i.end.split(':')
+            if (Start[0] <= hoursNow && End[0] >= hoursNow) {
+                if (Start[0] == hoursNow && End[0] == hoursNow) {
+                    if (Start[1] <= minutesNow && End[1] >= minutesNow) {
+
+                        result = {res: (((End[1]) - minutesNow) * 60 - secondsNow), color: i ? i.color : "black"}
+
+
+                    }
+                }
+
+                if (Start[0] < hoursNow && End[0] > hoursNow) {
+
+                    result = {
+                        res: ((End[0] - hoursNow) * 60 * 60 - minutesNow * 60) + (End[1] * 60 - secondsNow),
+                        color: i ? i.color : "black"
+                    }
+
+                }
+                if (Start[0] < hoursNow && End[0] == hoursNow) {
+                    if (End[1] > minutesNow) {
+                        result = {res: ((End[1] - minutesNow) * 60 - secondsNow), color: i ? i.color : "black"}
+
+                    }
+                }
+                if (Start[0] == hoursNow && End[0] > hoursNow) {
+                    if (Start[1] < minutesNow) {
+                        result = {
+                            res: ((End[0] - hoursNow) * 60 * 60 - minutesNow * 60) + ((End[1]) * 60 - secondsNow),
+                            color: i ? i.color : "black"
+                        }
+
+
+                    }
+                }
+
+            }
+
+        })
+        return result && result.res > 0 ? result : undefined
+    }
+    return <div className="App">
+        <audio id='audio' src={uvd} >
+        </audio>
+        <div className="clock" style={{color: color}}>
+            <div className="hours">
+                <div className="first">
+                    <div className="number">{Math.floor(Math.floor(time / 60 / 60) / 10) || '0'}</div>
+                </div>
+                <div className="second">
+                    <div className="number">{Math.floor(Math.floor(time / 60 / 60) % 10) || '0'}</div>
+                </div>
+            </div>
+            <div className="tick">:</div>
+            <div className="minutes">
+                <div className="first">
+                    <div className="number">{Math.floor((Math.floor(time / 60) % 60) / 10 || '0')}</div>
+                </div>
+                <div className="second">
+                    <div className="number">{Math.floor((Math.floor(time / 60) % 60) % 10 || '0')}</div>
+                </div>
+            </div>
+            <div className="tick">:</div>
+            <div className="minutes">
+                <div className="first">
+                    <div className="number">{Math.floor(Math.floor(time) % 60 / 10 || '0')}</div>
+                </div>
+                <div className="second">
+                    <div className="number">{Math.floor(Math.floor(time) % 60 % 10 || '0')}</div>
+                </div>
+            </div>
+
+        </div>
+        <img src={lavina} className="lavina"/>
+        <div className={cls}>
+            <label htmlFor="appt">Trenirovka vaqtini tanlang</label>
+
+            <input type="time" id="appt" name="appt"
+                   min="09:00" max="18:00" onChange={e => {
+                setTtime(e.target.value)
+            }}/>
+
+            <small>09:00 dan 18:00 gacha vaqni tanlang</small>
+            <label htmlFor="muz"> Trenirovka uchun musiqa tanlang </label>
+            <input type="file" id='muz' onChange={e => {
+
+                if(!sound.paused){
+                    sound.src = URL.createObjectURL(e.target.files[0]);
+                    sound.play()
+                }else{
+                    sound.src = URL.createObjectURL(e.target.files[0]);
+                }
+
+            }} accept=".mp3,audio/*"/>
+        </div>
+
+
+        <div className='check'> Trenirovka <input type="checkbox" onChange={e => {
+            if (e.currentTarget.checked) {
+                setCls('yes')
+
+            } else {
+                setCls('none')
+                setTtime('08:00')
+                setAlert('O\'chirildi')
+                setTimeout(()=>{
+                    setAlert('')
+                },2000)
+                !sound.paused && sound.load()
+            }
+        }}/></div>
+        <div className='alert'>{Alert}</div>
+        <audio id="sound"  src={music}></audio>
+
+    </div>
+}
+
+export default App;
---
+++ src/static/data.js
@@ -0,0 +1,48 @@
+import music1 from './music1.mp3'
+import music2 from './music2.mp3'
+import music3 from './music3.mp3'
+import music4 from './music4.mp3'
+import music5 from './music5.mp3'
+
+const data = [
+
+    {start: "08:15", end: "08:40", color: "grey"},
+    {start: "08:40", end: "08:45", color: "green"},
+    {start: "08:45", end: "09:10", color: "grey"},
+    {start: "09:10", end: "09:15", color: "green"},
+    {start: "09:15", end: "09:40", color: "grey"},
+    {start: "09:40", end: "09:45", color: "green"},
+    {start: "09:45", end: "10:10", color: "grey"},
+    {start: "10:10", end: "10:15", color: "green"},
+    {start: "10:15", end: "10:40", color: "grey"},
+    {start: "10:40", end: "10:45", color: "green"},
+    {start: "10:45", end: "11:10", color: "grey"},
+    {start: "11:10", end: "11:15", color: "green"},
+    {start: "11:15", end: "11:40", color: "grey"},
+    {start: "11:40", end: "11:45", color: "green"},
+    {start: "11:45", end: "12:10", color: "grey"},
+    {start: "12:10", end: "12:15", color: "green"},
+    {start: "12:15", end: "12:40", color: "grey"},
+
+    {start: "13:35", end: "14:00", color: "grey"},
+    {start: "14:00", end: "14:05", color: "green"},
+    {start: "14:05", end: "14:30", color: "grey"},
+    {start: "14:30", end: "14:35", color: "green"},
+    {start: "14:35", end: "15:00", color: "grey"},
+    {start: "15:00", end: "15:05", color: "green"},
+    {start: "15:05", end: "15:30", color: "grey"},
+    {start: "15:30", end: "15:35", color: "green"},
+    {start: "15:35", end: "16:00", color: "grey"},
+    {start: "16:00", end: "16:05", color: "green"},
+    {start: "16:05", end: "16:30", color: "grey"},
+    {start: "16:30", end: "16:35", color: "green"},
+    {start: "16:35", end: "17:00", color: "grey"},
+    {start: "17:00", end: "17:05", color: "green"},
+    {start: "17:05", end: "17:30", color: "grey"},
+    {start: "17:30", end: "17:35", color: "green"},
+    {start: "17:35", end: "18:00", color: "grey"},
+
+]
+export const musicData = [music1,music2, music3, music4, music5]
+
+export default data
